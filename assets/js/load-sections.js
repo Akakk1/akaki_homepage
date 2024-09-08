b71609ad5a6213d2ae0f3be8ae1f1215495e4450
朱俊桥 <135806514+Akakk1@users.noreply.github.com>
2024-09-08 17:31:03 +0800

    Add files via upload

---
+++ assets/js/load-sections.js
@@ -0,0 +1,13 @@
+// assets/js/load-sections.js
+
+// JavaScript to dynamically load sections
+document.querySelectorAll('.left-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        const section = this.getAttribute('data-section');
+        fetch(`/akaki_homepage/_includes/sections/${section}.html`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('info-display').innerHTML = data;
+        });
+    });
+});
