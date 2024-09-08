986c023de4bbddf449a8f7600cf7de4ceedd99a6
朱俊桥 <135806514+Akakk1@users.noreply.github.com>
2024-09-08 17:44:33 +0800

    Update load-sections.js

--- assets/js/load-sections.js
+++ assets/js/load-sections.js
@@ -4,7 +4,7 @@
 document.querySelectorAll('.left-btn').forEach(button => {
     button.addEventListener('click', function() {
         const section = this.getAttribute('data-section');
-        fetch(`https://github.com/Akakk1/akaki_homepage/blob/main/_includes/sections/${section}.html`)
+        fetch(`https://akakk1.github.io/akaki_homepage/_includes/sections/${section}.html`)
         .then(response => response.text())
         .then(data => {
             document.getElementById('info-display').innerHTML = data;
